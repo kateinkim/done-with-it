10926ed2d255781bf881b2fcaa937e0fb2a393b3
Kate Kim <kateinkim@gmail.com>
2020-06-24 14:04:39 -0700

    added login and logout method

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
 import navigationTheme from "./navigation/navigationTheme";
 import AppNavigator from "./navigation/AppNavigator";
 import OfflineNotice from "./components/OfflineNotice";
+import AuthNavigator from "./navigation/AuthNavigator";
+import AuthContext from "./auth/context";
 
 export default function App() {
+  const [user, setUser] = useState();
   return (
-    <>
+    <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
       <NavigationContainer theme={navigationTheme}>
-        <AppNavigator />
+        {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
-    </>
+    </AuthContext.Provider>
   );
 }
 
